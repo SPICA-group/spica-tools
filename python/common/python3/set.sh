92e7cd1beb8d505e171914ea05e610c3ba19d78d
yskmiyazaki <y.miyazaki@okayama-u.ac.jp>
2022-06-15 15:37:44 +0900

    changed set.sh and bin directory

--- python/common/python3/set.sh
+++ python/common/python3/set.sh
@@ -3,9 +3,5 @@
 # Usage: source set.sh
 # Python3 must be in your path.
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-if [ ! -e $SCRIPT_DIR/bin ];then
-  mkdir $SCRIPT_DIR/bin
-  ln -s $SCRIPT_DIR/src/cg_spica.py $SCRIPT_DIR/bin/cg_spica
-fi
 export PATH=$PATH:$SCRIPT_DIR/bin
 source $SCRIPT_DIR/src/spica-completion.sh
